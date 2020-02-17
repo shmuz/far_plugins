9a2342141526d99472400f097b286309b5135544
Shmuel Zeigerman <solomuz0@gmail.com>
2020-02-17 19:05:26 +0200

    LF4Ed v3.9.1

--- far_plugins/luafar4editor/build/define.lua
+++ far_plugins/luafar4editor/build/define.lua
@@ -1,11 +1,11 @@
 -- Definitions
 VER_MAJOR = "3"
 VER_MINOR = "9"
-VER_MICRO = "0"
+VER_MICRO = "1"
 
 MINFARVERSION = "{ 3, 0, 0, 4425 }"
 
-COPYRIGHT = "Shmuel Zeigerman, 2007-2018"
+COPYRIGHT = "Shmuel Zeigerman, 2007-2020"
 
 -- Derivative values --
 VER_STRING = VER_MAJOR.."."..VER_MINOR.."."..VER_MICRO
