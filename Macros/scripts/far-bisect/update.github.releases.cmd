fb966a5ff6c09067927f486abaa1769aefe69286
johnd0e <1838643+johnd0e@users.noreply.github.com>
2023-06-04 16:53:18 +0200

    far-bisect: improve update.github.releases.cmd

--- Macros/scripts/far-bisect/update.github.releases.cmd
+++ Macros/scripts/far-bisect/update.github.releases.cmd
@@ -1,6 +1,20 @@
 @echo off
-SET GH=gh
-SET max=15
-echo Recreating github.releases database...
-del github.releases
-for /l %%x in (1, 1, %max%) do echo %%x/%max% & %GH% api -X GET "repos/FarGroup/FarManager/releases?page=%%x&per_page=100" --jq ".[] | .name, (.assets.[].browser_download_url | select(.|test(\"7z$\") and (test(\"\\.pdb\") or test(\"ARM64\")|not)))">>github.releases
+set file=github.releases
+set GH=gh
+set max=100
+del %file%
+echo Recreating %file% database...
+
+setlocal enabledelayedexpansion
+set lastsize=0
+for /l %%x in (1, 1, %max%) do (
+  echo %%x..
+  %GH% api -X GET "repos/FarGroup/FarManager/releases?page=%%x&per_page=100" --jq ".[] | .name, (.assets.[].browser_download_url | select(.|test(\"7z$\") and (test(\"\\.pdb\") or test(\"ARM64\")|not)))">>%file%
+  call :setsize %file%
+  if !size!==!lastsize! goto :eof
+  set lastsize=!size!
+)
+goto :eof
+
+:setsize
+set size=%~z1
